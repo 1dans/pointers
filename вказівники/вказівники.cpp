677c8b153a876158c6366301ec9bf3d8e15ac7dc
1dans <idansuwu@gmail.com>
2024-12-14 17:55:24 +0200

    new, delete, const *int, int *const

--- вказівники/вказівники.cpp
+++ вказівники/вказівники.cpp
@@ -1,25 +1,26 @@
 ﻿#include <iostream>
 #include<algorithm>
+#include <vector>
 using namespace std;
 
-void fill(int* ptr, int size) {
-	for (int i = 0; i < size; ++i) {
-		*(ptr + i) = rand() % 11;
-	}
-}
-
+//void fill(int* ptr, int size) {
+//	for (int i = 0; i < size; ++i) {
+//		*(ptr + i) = rand() % 11;
+//	}
+//}
+//
 void show(int* ptr, int size) {
 	for (int i = 0; i < size; ++i) {
 		cout << *(ptr + i) << ' ';
 	}
 	cout << endl;
 }
-
-void replace(int* ptr, int size) {
-	for (int i = 0; i < size; ++i) {
-		if (i % 2 == 0) swap(*(ptr + i), *((ptr + i) + 1));
-	}
-}
+//
+//void replace(int* ptr, int size) {
+//	for (int i = 0; i < size; i+=2) {
+//		swap(*(ptr + i), *((ptr + i) + 1));
+//	}
+//}
 //int summ(int* ptr, int size) {
 //	int s=0;
 //	for (int i = 0; i < size; ++i) {
@@ -35,7 +36,29 @@ void replace(int* ptr, int size) {
 //	else return '0';
 //}
 
+//void userFill(int* ptr, int size) {
+//	cout << "Заповніть масив "<< size<<" числами";
+//	int* even = nullptr, *odd=nullptr;
+//
+//	for (int i = 0; i < size; ++i) { 
+//		cin >> *(ptr + i); 
+//	}
+//	
+//}
+//
+//void deleteOdd(int* ptr, int size) {
+//	int* ar = new int[size];
+//	for (int i = 0; i < size; ++i) if (*(ptr + i) % 2 != 0) ;
+//	show(ptr, size);
+//}
+//
+//void deleteEven(int* ptr, int size) {
+//	for (int i = 0; i < size; ++i) if (*(ptr + i) % 2 == 0);
+//	show(ptr, size);
+//}
+
 int main() {
+	srand(time(nullptr));
 	system("chcp 1251>null");
 	cout << "вказівники" << endl;
 	//int a = 3, b = 5;
@@ -65,11 +88,37 @@ int main() {
 	cout << "Введіть число: ";
 	cin >> num;
 	cout << sign(num);*/
-	const int size = 10;
-	int ar[size];
-	int* p = ar;
-	fill(p, size);
-	show(p, size);
-	replace(p, size);
-	show(p, size);
+	
+	//int size;
+	//cout << "Введіть розмір масиву: ";
+	//cin >> size;
+	//if (size % 2 != 0) --size;
+	//int *ptr = new int[size]; // виділення пам'яти для динамічного масиву
+	//fill(ptr, size);
+	//show(ptr, size);
+	//replace(ptr, size);
+	//show(ptr, size);
+	//delete[]ptr;
+
+	//int size, ans;
+	//cout << "Введіть розмір масиву: ";
+	//cin >> size;
+	//int* ptr = new int[size];
+	//userFill(ptr, size);
+	//cout << "Які числа ви би хотіли видалити? 0 - парні, 1 - непарні "<<endl;
+	//
+	//cin >> ans;
+	//if (ans == 0) deleteEven(ptr, size);
+	//else if (ans == 1) deleteOdd(ptr, size);
+	//else cout << "Цього варіанту немає.";
+	// delete[]ptr;
+	//const int* p = nullptr; //вказівник на константу
+	//cout << p << endl;
+	//int x = 5, y=7;
+	//p = &x;
+	//cout << p << endl;
+	//p = &y;
+	//cout << p << endl;
+	int* const p = nullptr; // констатний вказівник
+
 }
